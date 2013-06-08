99e804c37e2cebee0ec76e79b2e656e8422cefd4
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-06-09 01:01:55 +0300

    server respond

--- try.go
+++ try.go
@@ -1,18 +1,7 @@
 package main
-
 import "fmt"
-
 func main() {
-  var first, second int
-
-  user := make([][]string, first)
-  for i := range user {
-    user[i] = make([]string, second)
-  }
-
-  fmt.Println(user[0][0])
+user := make(map[string]string)
+user["name"] = "slavik"
+fmt.Println(user["name"])
 }
-
-//user := make(map[string][string]string)
-//user["first"]["name"] = "slavik"
-//fmt.Println(user["first"]["name"])
--- web.go
+++ web.go
@@ -1,14 +1,30 @@
 package main
 
 import (
-    "github.com/hoisie/web"
+  "github.com/hoisie/web"
+  "fmt"
+  "io/ioutil"
+  "os"
 )
 
-func hello(val string) string {
-    return "hello " + val
+func check(e error) {
+  if e != nil {
+    panic(e)
+  }
+}
+
+func output(file_name string) string {
+  pwd, err := os.Getwd()
+  check(err)
+
+  fmt.Println(pwd)
+  dat, err := ioutil.ReadFile(pwd + "/" + file_name)
+  check(err)
+
+  return string(dat)
 }
 
 func main() {
-    web.Get("/(.*)", hello)
-    web.Run("0.0.0.0:9999")
+  web.Get("/(.*)", output)
+  web.Run("0.0.0.0:9999")
 }
