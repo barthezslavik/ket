9654893020291468a28c3ff234774a5ec58e8f5f
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-06-13 03:00:31 +0300

    correct map

--- json_parser.go
+++ json_parser.go
@@ -1,9 +1,10 @@
 package main
 import (
-  "os"
-  "io/ioutil"
-  //"fmt"
-  "strings"
+  //"os"
+  //"io/ioutil"
+  "fmt"
+  "encoding/json"
+  //"strings"
 )
 
 func check(e error) {
@@ -13,9 +14,20 @@ func check(e error) {
 }
 
 func main() {
-  contents,_ := ioutil.ReadFile(os.Args[1]+".ket")
-  var lines = strings.Split(string(contents), "\n")
-  println(lines[0])
+  //contents,_ := ioutil.ReadFile(os.Args[1]+".ket")
+  //var lines = strings.Split(string(contents), "\n")
+
+  //first := //
+
+second := map[string]interface{} { "name": "slavik", "age": 21 }
+first := map[string]interface{} { "second":second }
+hello := map[string]interface{} { "super": "hero"}
+someone := map[string]interface{} { "hello":hello }
+user := map[string]interface{} { "first":first, "someone":someone }
+
+j, _ := json.Marshal(user)
+
+fmt.Println(string(j))
 }
 
 /*
@@ -28,18 +40,5 @@ user
   someone
     hello
       super:hero
-
-{"user":
-  {"first":
-    {"second":
-      {"name":"slavik","age":"21"}
-    },
-  {"someone":
-      {"hello":
-      {"super":"hero"}
-    }
-  }
-  }
-}
 
 */
