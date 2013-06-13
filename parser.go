c2860bfc48d4182a57852cc7455f7a1bf3e7efa3
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-06-13 18:46:30 +0300

    rename

--- parser.go
+++ parser.go
@@ -1,10 +1,10 @@
 package main
 import (
-  //"os"
-  //"io/ioutil"
-  "fmt"
+  "os"
+  //"fmt"
   //"encoding/json"
   //"strings"
+  //"bufio"
 )
 
 func check(e error) {
@@ -17,7 +17,7 @@ func main() {
   //contents,_ := ioutil.ReadFile(os.Args[1]+".ket")
   //var lines = strings.Split(string(contents), "\n")
 
-  a := make([]string, 1)
+  a := make([]string, 0)
   a = append(a, `second  := map[string]interface{} { "name": "slavik", "age": 21 }`)
   a = append(a, `hello   := map[string]interface{} { "super": "hero"}`)
   a = append(a, `someone := map[string]interface{} { "hello": hello }`)
@@ -25,23 +25,32 @@ func main() {
   a = append(a, `user    := map[string]interface{} { "first": first, "someone": someone }`)
 
   for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-     a[i], a[j] = a[j], a[i]
+    a[i], a[j] = a[j], a[i]
   }
 
+  //for i := 0; i < len(a); i++ {
+  //  fmt.Println(a[i])
+    //err := ioutil.WriteFile("/tmp/www", string(a[i]), 0644)
+    //check(err)
+  //}
   //j, _ := json.Marshal(user)
 
-  fmt.Println(a)
+  //fmt.Println(a)
+
+  f, err := os.Create("/tmp/dat2")
+  check(err)
+  f.WriteString("writes\n")
 }
 
 /*
 
 user
-  first
-    second
-      name:slavik
-      age:21
-  someone
-    hello
-      super:hero
+first
+second
+name:slavik
+age:21
+someone
+hello
+super:hero
 
 */
