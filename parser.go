5afdee3f3fe1319e8bd5db10ad6dfbbe182de909
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-06-13 19:07:22 +0300

    cleanup

--- parser.go
+++ parser.go
@@ -1,10 +1,6 @@
 package main
 import (
   "os"
-  //"fmt"
-  //"encoding/json"
-  //"strings"
-  //"bufio"
 )
 
 func check(e error) {
@@ -14,32 +10,37 @@ func check(e error) {
 }
 
 func main() {
-  //contents,_ := ioutil.ReadFile(os.Args[1]+".ket")
-  //var lines = strings.Split(string(contents), "\n")
-
   a := make([]string, 0)
   a = append(a, `
-  second  := map[string]interface{} { "name": "slavik", "age": 21 }`)
+  }`)
   a = append(a, `
-  hello   := map[string]interface{} { "super": "hero"}`)
+  println(user)`)
+  a = append(a, `
+  user    := map[string]interface{} { "first": first, "someone": someone }`)
   a = append(a, `
   someone := map[string]interface{} { "hello": hello }`)
   a = append(a, `
   first   := map[string]interface{} { "second": second }`)
   a = append(a, `
-  user    := map[string]interface{} { "first": first, "someone": someone }`)
+  second  := map[string]interface{} { "name": "slavik", "age": 21 }`)
+  a = append(a, `
+  hello   := map[string]interface{} { "super": "hero"}`)
+  a = append(a, `
+  func main() {`)
+  a = append(a, `
+  package main`)
 
   for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
     a[i], a[j] = a[j], a[i]
   }
 
-  f, err := os.Create("/tmp/dat2")
+  f, err := os.Create(os.Args[1]+".go")
   check(err)
   for i := 0; i < len(a); i++ {
     f.WriteString(a[i])
   }
-  //j, _ := json.Marshal(user)
 
+  //j, _ := json.Marshal(user)
   //fmt.Println(a)
 
 }
