71c8bb2068181b7e15b086bcd727c1865223cca6
Vyacheslav Loginov <vyacheslav.loginov@nasctech.com>
2013-06-13 13:32:55 +0300

    use slice

--- json_parser.go
+++ json_parser.go
@@ -17,14 +17,12 @@ func main() {
   //contents,_ := ioutil.ReadFile(os.Args[1]+".ket")
   //var lines = strings.Split(string(contents), "\n")
 
-  //first := //
-
-  var a [5]string
-  a[0] = `second  := map[string]interface{} { "name": "slavik", "age": 21 }`
-  a[1] = `hello   := map[string]interface{} { "super": "hero"}`
-  a[2] = `someone := map[string]interface{} { "hello": hello }`
-  a[3] = `first   := map[string]interface{} { "second": second }`
-  a[4] = `user    := map[string]interface{} { "first": first, "someone": someone }`
+  a := make([]string, 1)
+  a = append(a, `second  := map[string]interface{} { "name": "slavik", "age": 21 }`)
+  a = append(a, `hello   := map[string]interface{} { "super": "hero"}`)
+  a = append(a, `someone := map[string]interface{} { "hello": hello }`)
+  a = append(a, `first   := map[string]interface{} { "second": second }`)
+  a = append(a, `user    := map[string]interface{} { "first": first, "someone": someone }`)
 
   for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
      a[i], a[j] = a[j], a[i]
